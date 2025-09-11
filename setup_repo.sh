079325e154fb98d7ed451bcdc35b4f4d69b12901
Nahom <nahom.atnafu@mnsu.edu>
2025-09-11 18:36:19 -0500

    Initial commit: ultrasonic Morse code decoder

---
+++ setup_repo.sh
@@ -0,0 +1,9 @@
+# Initialize git repo
+git init
+git add .
+git commit -m "Initial commit: ultrasonic Morse code decoder"
+
+# Add remote (replace YOUR_USERNAME and REPO_NAME)
+git remote add origin https://github.com/Nahomatnafu/morse-audio-decoder.git
+git branch -M main
+git push -u origin main
